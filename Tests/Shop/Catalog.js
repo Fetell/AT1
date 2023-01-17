018cd99fd92d2e537c14ed137f0dd45dadfea515
Fetell <fetell.eee@gmail.com>
2023-01-17 15:16:51 +0300

    Module 4 Lesson 1 homework

---
+++ Tests/Shop/Catalog.js
@@ -0,0 +1,20 @@
+module.exports = {
+    'Check Store title': function (browser) {
+        browser
+            .url('http://testshop.sedtest-school.ru/')
+            .waitForElementVisible('.col-md-3', 'Заголовок загружен');
+        browser.assert.titleEquals("TestGym");
+        browser.assert.textContains(".col-md-3", "Каталог");
+
+        browser.expect.title().to.contain('TestGym');
+        browser.expect.element('.col-md-3').text.to.contain('Каталог');
+
+        browser.expect.title().to.equal('TestGym');
+        browser.expect.element('.col-md-3').text.to.equal('Каталог');
+
+        browser.expect.title().to.match(/TestGym/);
+        browser.expect.element('.col-md-3').text.to.match(/Каталог/);
+
+        browser.end();
+    }
+};
---
+++ Tests/Wiki/WikiTitleCheck.js
@@ -0,0 +1,20 @@
+module.exports = {
+    'Check Wiki title': function (browser) {
+        browser
+            .url('http://ru.wikipedia.org/wiki/Wiki')
+            .waitForElementVisible('#firstHeading', 'Заголовок загружен');
+        browser.assert.titleEquals("Вики — Википедия");
+        browser.assert.textContains("#firstHeading", "Вики");
+
+        browser.expect.title().to.contain('Вики — Википедия');
+        browser.expect.element('#firstHeading').text.to.contain('Вики');
+
+        browser.expect.title().to.equal('Вики — Википедия');
+        browser.expect.element('#firstHeading').text.to.equal('Вики');
+
+        browser.expect.title().to.match(/Вики — Википедия/);
+        browser.expect.element('#firstHeading').text.to.match(/Вики/);
+
+        browser.end();
+    }
+};
